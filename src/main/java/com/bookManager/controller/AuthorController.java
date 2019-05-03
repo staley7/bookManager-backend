c783f39629f43c892cc46336e81faa98f6b1d0f1
lstaley <lstaley@AMES-LENOVO1774.xpx.intra>
2019-05-03 14:37:24 -0500

    updated Author controller.

--- src/main/java/com/bookManager/controller/AuthorController.java
+++ src/main/java/com/bookManager/controller/AuthorController.java
@@ -8,12 +8,17 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.data.domain.Pageable;
-import com.bookManager.dao.model.Author;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+
+import com.bookManager.dto.AuthorDto;
+import com.bookManager.hateoas.AuthorResourceAssembler;
 import com.bookManager.service.AuthorService;
 
 @RestController()
@@ -22,14 +27,32 @@ public class AuthorController {
 
 	@Autowired
 	private AuthorService authorService;
-	
+
+	@Autowired
+	AuthorResourceAssembler authorAssembler;
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(AuthorController.class);
-	
-	//TODO
+
 	@CrossOrigin
-	@RequestMapping(value =  "/authors", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
-	public Set<Author> getAuthors(@RequestParam(required = false) Integer pageNumber, @RequestParam(required = false) Integer pageSize) {
-		return authorService.getAuthors(pageNumber, pageSize);
+	@RequestMapping(value = "/authors", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public Resources<AuthorDto> getAuthors(@RequestParam(required = false) Integer pageNumber,
+			@RequestParam(required = false) Integer pageSize) {
+		Set<AuthorDto> authors = authorService.getAuthors(pageNumber, pageSize);
+		return new Resources<>(authorAssembler.toResources(authors), ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(AuthorController.class).getAuthors(pageNumber, pageSize)
+				).withSelfRel());
 	}
-	
+
+	@CrossOrigin
+	@RequestMapping(value = "/author", method = RequestMethod.PUT, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public AuthorDto saveAuthor(@RequestBody AuthorDto author) {
+		LOGGER.info("save Author");
+		return authorAssembler.toResource(authorService.saveAuthor(author));
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/author/{id}", method = RequestMethod.GET)
+	public AuthorDto getAuthor(@PathVariable Long id) {
+		return authorAssembler.toResource(authorService.getAuthor(id));
+	}
+
 }
